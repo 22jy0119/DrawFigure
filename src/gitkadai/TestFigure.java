8393ced8f38b7a95efc92c574b72ffb4ed888e17
22jy0119 <22jy0119@jec.ac.jp>
2024-01-06 16:10:36 +0900

    for文を追加

--- src/gitkadai/TestFigure.java
+++ src/gitkadai/TestFigure.java
@@ -5,6 +5,10 @@ public class TestFigure {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("こんにちは");
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
